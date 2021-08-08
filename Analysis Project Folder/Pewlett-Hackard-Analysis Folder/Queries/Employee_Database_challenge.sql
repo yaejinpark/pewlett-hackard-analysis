f5f7c14b0d59becdccbcb3ce79133a447367a87b
yaejinpark <yaejinpark@Yae-Jins-MacBook-Pro.local>
2021-08-07 19:48:51 -0700

    deliverable 1 done

---
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,41 @@
+-- Deliverable 1
+
+-- Create a Retirement Titles table that holds all the titles of current employees 
+-- who were born between January 1, 1952 and December 31, 1955
+
+-- # 1-7
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+-- Create a new table using the INTO clause.
+-- Join both tables on the primary key.
+-- Filter the data on the birth_date column to retrieve the employees who were born between 
+-- 1952 and 1955. Then, order by the employee number.
+-- Export the Retirement Titles table from the previous step as 
+-- retirement_titles.csv and save it to your Data folder
+
+select e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+into retirement_titles
+from employees as e
+inner join titles as ti
+on e.emp_no = ti.emp_no
+where e.birth_date between '1952-01-01' and '1955-12-31'
+order by emp_no asc;
+
+-- # 8-14
+-- Filter repeating names for latest positions
+SELECT DISTINCT ON (emp_no) emp_no,first_name,last_name,title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no asc, to_date DESC;
+
+-- # 15-21
+-- retrieve the number of employees by their most recent job title who are about to retire.
+select count(title),title
+into retiring_titles
+from unique_titles 
+group by title 
+order by 1 desc;
+
+-- -------------------------------------------------------------------------------------------
+
+-- Deliverable 2
--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -45,3 +45,73 @@ into retirement_info
 from employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 and (hire_date between '1985-01-01' and '1988-12-31');
+
+-- Joining retirement_info and dept_emp tables
+select ri.emp_no,ri.first_name, ri.last_name,dept_emp.to_date
+from retirement_info as ri
+left join dept_emp
+on ri.emp_no = dept_emp.emp_no;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Saving retiring employee info
+select ri.emp_no,ri.first_name, ri.last_name,de.to_date
+into current_emp
+from retirement_info as ri
+left join dept_emp as de
+on ri.emp_no = de.emp_no
+where de.to_date = ('9999-01-01');
+
+-- join the current_emp and dept_emp tables
+select count(ce.emp_no), de.dept_no
+from current_emp as ce
+left join dept_emp as de
+on ce.emp_no = de.emp_no
+group by de.dept_no
+order by de.dept_no;
+
+-- Update the code block to create a new table, then export it as a CSV.
+select count(ce.emp_no), de.dept_no
+into current_emp_count
+from current_emp as ce
+left join dept_emp as de
+on ce.emp_no = de.emp_no
+group by de.dept_no
+order by de.dept_no;
+
+-- emp_info
+SELECT e.emp_no,e.first_name,e.last_name,e.gender,s.salary,de.to_date
+into emp_info
+from employees as e
+inner join salaries as s
+on e.emp_no = s.emp_no
+inner join dept_emp as de
+on e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+-- manager_info
+select dm.dept_no,d.dept_name,ce.first_name,ce.last_name,dm.from_date,dm.to_date
+into manager_info
+from dept_manager as dm
+inner join departments as d
+on dm.dept_no = d.dept_no
+inner join current_emp as ce
+on dm.emp_no = ce.emp_no;
+
+-- dept_info
+SELECT ce.emp_no,ce.first_name,ce.last_name,d.dept_name
+into dept_info
+from departments as d
+inner join dept_emp as de
+on d.dept_no = de.dept_no
+inner join current_emp as ce
+on ce.emp_no = de.emp_no;
